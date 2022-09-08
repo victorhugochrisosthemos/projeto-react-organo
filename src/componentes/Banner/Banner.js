fbd4bbf39d844270bd11da31ad7de7a62b2abad0
Victor Chrisosthemos <victor_lampert@outlook.com>
2022-09-08 14:29:35 -0300

    Criando commit do projeto Organo feito em React

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,14 @@
+import { Fragment } from 'react'
+import './Banner.css'
+
+export const Banner = () => {
+    //JSX
+    return (
+        <>
+            <header className="banner">
+                <img src="\imagens/banner.png" alt="O banner principal da página do Organo"/>
+            </header>
+        </>
+    )
+}
+
